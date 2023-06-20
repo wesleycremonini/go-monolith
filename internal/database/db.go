6d7bee6b603a989706269c49a5146ebe2bdab930
wesleycremonini <wesleycremonini0@gmail.com>
2023-06-19 21:28:35 -0300

    chore:change db to postgres

---
+++ internal/database/db.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type DB struct {
+	*pgxpool.Pool
+}
+
+// connectDB tries to open a connection and returns it.
+func Connect(dsn string) (*DB, error) {
+	db, err := pgxpool.New(context.TODO(), dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DB{db}, nil
+}
